c8fd16a35d6ba655eb1d0d880ea46ee99dcb07c0
Yaying <yliangli20@stuy.edu>
2021-09-23 00:27:45 -0400

    removed unnecessary strip of new lines:

--- printNames.py
+++ printNames.py
@@ -1,19 +1,18 @@
 import random
 
 def printName():
-	file_name = input("Input the .txt file containing the roster: ")
-	my_file = open(file_name, "r")
-	
-	list_names = []
-	#source: kite.com
-	for line in my_file:
-		stripped_line = line.strip()
-		line_list = stripped_line.split()
-		list_names.append(line_list)
-		
-	my_file.close()
-	
-	print(" ".join(list_names[random.randint(0, len(list_names)-1)]))
-	
-	
+   file_name = input("Input the .txt file containing the roster: ")
+   my_file = open(file_name, "r")
+   
+   list_names = []
+   #source: kite.com
+   for line in my_file:
+           line_list = line.split()
+           list_names.append(line_list)
+      
+   my_file.close()
+
+   print(" ".join(list_names[random.randint(0, len(list_names)-1)]))
+   
+   
 printName();
