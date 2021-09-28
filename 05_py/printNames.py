289f708342d3ea963d59f1e3826da609438834a9
Yaying <yliangli20@stuy.edu>
2021-09-28 07:11:10 -0400

    moved names from .txt file to dictionary of names

--- 05_py/printNames.py
+++ 05_py/printNames.py
@@ -1,16 +1,31 @@
 #Yaying Liang Li, Raymond Yeung, Kevin Cao
 #SoftDev
 #K<05> -- Amalgamation of Python Random Name Generator Code
-#2021-09-24
+#2021-09-27
 
 import random
 
 def printName():
-   option = input("Input the number period's roster you want to access: ")
-   if option == "1":
-       file_name = "pd1.txt"
-   elif option == "2":
-       file_name = "pd2.txt"
+   names = {
+      'pd1': ["Alejandro Alonso", "Aryaman Goenka", "Christopher Liu",
+              "Deven Maheshwari", "Emma Buller", "Ella Krechmer",
+              "Gavin McGinley", "Haotian Gan", "Ivan Lam", "Ishraq Mahid",
+              "Ivan Mijacika", "Julia Nelson", "Lucas Lee", "Lucas Tom Wang",
+              "Michelle Lo", "Oscar Wang", "Rayat Roy", "Renggeng Zheng",
+              "Shriya Anand", "Shyne Choi", "Sadid Ethun", "Tomas Acuna",
+              "Theodore Fahey", "Tina Nguyen", "Tami Takada", "William Chen",
+              "Yusuf Elsharawy", "Zhaoyu Lin"],
+      
+      'pd2': ["Alif Abdullah", "Andrew Juang", "Andy Lin", "Austin Ngan",
+              "Annabel Zhang", "Cameron Nelson", "David Chong",
+              "Daniel Sooknana", "Eric Guo", "Eliza Knapp", "Hebe Huang",
+              "Han Zhang", "Yoonah Chang", "Joshua Kloepfer", "Josephine Lee",
+              "Mark Zhu", "Jonathan Wu", "Jesse Xie", "Justin Zou",
+              "Kevin Cao", "Liesel Wong", "Michael Borczuk", "Noakai Aronesty",
+              "Patrick Ging", "Qina Liu", "Roshani Shrestha", "Rachel Xiao",
+              "Raymond Yeung", "Sophie Liu", "Shadman Rakib", "Thomas Yu",
+              "Wenhao Dong", "Yaying Liang Li", "Yuqing Wu"]
+   }
 
    my_file = open(file_name, "r")
    
