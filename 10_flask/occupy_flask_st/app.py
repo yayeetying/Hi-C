257d86cdbd99b4425a4b51baec8890851a4c4a9c
Yaying <yliangli20@stuy.edu>
2021-10-06 06:42:07 -0400

    added K06 code to K10 code; added printing out stuff onto webpage

--- 10_flask/occupy_flask_st/app.py
+++ 10_flask/occupy_flask_st/app.py
@@ -3,10 +3,48 @@
 #K10 -- first flask website, serves the outputs of K06's job picker
 #2021-10-05
 
+#We tried tinkering with importing the file from K06 to here, but
+#it didn't seem to work. End result: we just ctrl c + ctrl v-ied
+#it here.
+
+#from flask_mobility import Mobility
+#import sys
+#sys.path.append('~/Desktop/SoftDev/Hi-C/06_py-csv')
+#from readCSVBetter import openFile
+
 from flask import Flask
-from K06 import jobDecider, getOccupations
+import csv
+import random
 
 app = Flask(__name__)
+
+def openFile():
+    info = {} #create new dict called info
+    
+    with open('occupations.csv',mode='r') as csvfile: #open file for reading; we can refer to it as csvfile
+        reader = csv.DictReader(csvfile) #reader is object of class csv
+        for row in reader: #every row is a dict; format: {'Job Class': <xx>, 'Percentage': <yy>}
+            #for first row in file, create key-value pair in info with format: 'Job Class': <xx>, 'Percentage': <yy>
+            #key is 'Job Class'; value is 'Percentage'
+            #for every other row, update existing key-value pair to include new value given
+            if row['Job Class'] != "Total":
+                info[row['Job Class']] = float(row['Percentage'])
+
+    return info;
+
+def getOccupations():
+    info = openFile()
+    keys = list(info.keys())
+    return keys
+
+def makeChoice():
+    info = openFile()
+    #make a randomized, weighted choice
+    #population are the occupations, weights are the percentages they should show up
+    result = random.choices(list(info.keys()), weights = info.values(), k=1)
+    stringResult = ''.join(result)
+    print(stringResult)
+    return stringResult
 
 @app.route("/")
 def job_decider_web():
@@ -14,6 +52,8 @@ def job_decider_web():
     occupationsString = ""
     for occupation in occupations:
         occupationsString += occupation+"<br>"
-    return "Hi-C: Yaying Liang Li, Andy Lin, Josephine Lee <br><br>Possible results:<br>"+occupationsString+"<br><br>"+"Occupation Chosen:<br><br>"+jobDecider()
-
-app.run()
+    return "Hi-C: Yaying Liang Li, Andy Lin, Josephine Lee <br><br> Possible results:<br>"+occupationsString+"<br><br>"+"Occupation Chosen:<br><br>"+makeChoice()
+    
+if __name__ == "__main__":  
+    app.debug = True       
+    app.run()
