227ff6225989d0ee1ff6a0f5e4daa80780204c34
Yaying <yliangli20@stuy.edu>
2021-10-06 05:50:33 -0400

    replaced manually reading in file with using dictreader

--- 06_py-csv/readCSV-better.py
+++ 06_py-csv/readCSV-better.py
@@ -6,17 +6,22 @@
 import random, csv
 
 def openFile():
-    keys = ["Job Occupations", "Percentages"]
-    #create new dict called info and initialize it with empty keys (no values in keys yet)
-    info = dict.fromkeys(keys)
+    info = {} #create new dict called info
     
     with open('occupations.csv',mode='r') as csvfile: #open file for reading; we can refer to it as csvfile
         reader = csv.DictReader(csvfile) #reader is object of class csv
         for row in reader: #every row is a dict; format: {'Job Class': <xx>, 'Percentage': <yy>}
-            print(row['Job Class'])
-            #every row's 'Job Class' will be added to info under the key "Job Occupations"
-            info["Job Occupations"] = row['Job Class']
+            #for first row in file, create key-value pair in info with format: 'Job Class': <xx>, 'Percentage': <yy>
+            #key is 'Job Class'; value is 'Percentage'
+            #for every other row, update existing key-value pair to include new value given
+            info[row['Job Class']] = float(row['Percentage'])
 
-    print(info)
+    #keys = list(info.keys())
+    #weights = info.values();
+
+    #make a randomized, weighted choice
+    #population are the occupations, weights are the percentages they should show up
+    result = random.choices(list(info.keys()), weights = info.values(), k=1)
+    print(result)
 
 openFile()
