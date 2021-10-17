5f7a2d106fdf4afdd66f6324179b4d405c6a91a8
Yaying <yliangli20@stuy.edu>
2021-10-17 18:29:14 -0400

    added headings and comments to python file

--- 14_form/app.py
+++ 14_form/app.py
@@ -1,6 +1,7 @@
-# Clyde 'Thluffy' Sinclair
-# SoftDev
-# Oct 2021 
+# Team threeCoffeePeanuts: Jesse Xie, Yaying Liang Li, Ryan Wang
+#SoftDev
+#K14 -- Form and Function
+#2021-10-14
 
 from flask import Flask             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
@@ -25,35 +26,49 @@ and/or current teammates understand what is going on.
 
 @app.route("/") #, methods=['GET', 'POST'])
 def disp_loginpage():
-    print("\n\n\n")
+    print("\n\n\n") #prints in terminal
     print("***DIAG: this Flask obj ***")
-    print(app)
+    print(app) #<Flask 'app'>
     print("***DIAG: request obj ***")
-    print(request)
+    print(request) #<Request 'http://127.0.0.1:5000/' [GET]>
     print("***DIAG: request.args ***")
-    print(request.args)
+    print(request.args) #ImmutableMultiDict([])
+    #empty dict cuz user hasn't submitted anything yet
+
+    #request.args['username'] doesn't exist yet, cuz user didn't submit username yet
     #print("***DIAG: request.args['username']  ***")
     #print(request.args['username'])
     print("***DIAG: request.headers ***")
-    print(request.headers)
+    print(request.headers) #Host: 127.0.0.1:5000
+
+    '''
+        form prompts user to enter username in webpage;
+        <name="username"> saves the user's inputted name under the key "username" in the ImmutableMultiDict
+        <form action="/auth"> after user submits, takes the user to <host>/auth?username=...
+    '''
     return render_template( 'login.html' )
 
 
 @app.route("/auth") # , methods=['GET', 'POST'])
 def authenticate():
-    print("\n\n\n")
+    print("\n\n\n") #prints in terminal
     print("***DIAG: this Flask obj ***")
     print(app)
-    print("***DIAG: request obj ***")
-    print(request)
+    print("***DIAG: request obj ***") #gives you actual http link of page you're on
+    print(request) #<Request 'http://127.0.0.1:5000/auth?username=ajdskdksa&sub1=Submit' [GET]>
     print("***DIAG: request.args ***")
-    print(request.args)
-    #print("***DIAG: request.args['username']  ***")
-    #print(request.args['username'])
+    print(request.args) #ImmutableMultiDict([('username', 'ajdskdksa'), ('sub1', 'Submit')])
+    print("***DIAG: request.args['username']  ***")
+    print(request.args['username']) #ajdskdksa
     print("***DIAG: request.headers ***")
-    print(request.headers)
+    print(request.headers) #Host: 127.0.0.1:5000
     
-    username = request.args['username']
+    username = request.args['username'] #access value of key 'username' in Dict
+    '''
+        use response.html as template
+        method (called in response.html) is actually the request.method (which is get)
+        username (called in response.html) is actually username (from this python file)
+    '''
     return render_template('response.html', method=request.method, username=username)  #response to a form submission
 
     
