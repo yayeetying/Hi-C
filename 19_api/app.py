6ce273949bc87e5644711e477a9008f46666f1c5
yayeetying <yliangli20@stuy.edu>
2021-11-23 16:59:55 -0500

    added JSON load and using dictionary in app.py

--- 19_api/app.py
+++ 19_api/app.py
@@ -2,12 +2,22 @@
 # SoftDev
 # Oct 2021
 
-from flask import Flask, render_template, urllib3, json
+from flask import Flask, render_template
+from urllib import request
+import json
+
 app = Flask(__name__)
 
 @app.route("/")
 def rest_demo():
-    http = urllib3.PoolManager
+    http = request.urlopen("https://api.nasa.gov/planetary/apod?api_key=qsb4nvuGri4tJe3q6REknzJbP5xO1OZnJBDfLKMG") #HTTP Response object (containing the JSON info?)
+    print(http)
+    j = json.load(http) #j is a dictionary of the JSON info
+    print(j)
+    link = j['url']
+    print(link)
+    return link
+    
 
 if __name__ == "__main__":
     app.debug = True
