2ce52326605357cbef148087e063be674993e13d
Yaying <yliangli20@stuy.edu>
2021-11-23 20:37:35 -0500

    added comments in code and notes

--- 19_api/app.py
+++ 19_api/app.py
@@ -1,6 +1,6 @@
-# Clyde 'Thluffy' Sinclair
+# Team fourCoffeePeanuts: Yaying Liang Li, Thomas Yu
 # SoftDev
-# Oct 2021
+# K19 -- A RESTful Joruney Skyward
 
 from flask import Flask, render_template
 from urllib import request
@@ -13,15 +13,16 @@ def rest_demo():
     http = request.urlopen("https://api.nasa.gov/planetary/apod?api_key=qsb4nvuGri4tJe3q6REknzJbP5xO1OZnJBDfLKMG") #HTTP Response object (containing the JSON info)
     print(http)
 
-    j = json.load(http) #j is a dictionary of the JSON info
+    j = json.load(http) #j is a dictionary (key-value pairs) of the JSON info
     print(j)
 
-    link = j['url']
+    link = j['url'] #get the value of the 'url' key
     print(link)
 
-    explanation = j['explanation']
+    explanation = j['explanation'] #get the value of the 'explanation' key
     print(explanation)
 
+    #render an html template with the picture (using url) and explanation
     return render_template("main.html", pic = link, description = explanation)
 
 
