406dd9a0f5366d7e24ae999bb654d58a051c0790
Yaying <yliangli20@stuy.edu>
2022-02-15 05:28:52 -0500

    skeleton

--- 30/clickdraw.js
+++ 30/clickdraw.js
@@ -7,6 +7,8 @@ var ctx = c.getContext("2d");
 //init global state var
 var mode = "rect";
 
+//arrow function -- more limited; param => expression
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
 //var toggleMode = function(e) {
 var toggleMode = (e) => {
   console.log("toggling...");
@@ -14,8 +16,30 @@ var toggleMode = (e) => {
     bToggler.innerHTML
   }
   else { //mode is "circle"
-    
+
   }
+}
+
+//traditional function
+var drawRect = function (e) {
+  var mouseX =
+  var mouseY =
+  console.log("mouseclick registered at ", mouseX, mouseY);
+}
+
+//var drawCircle = funciton(e) {
+var drawCircle = (e) => {
+  console.log("mouseclick registered at ", mouseX, mouseY);
+}
+
+//var draw = function(e) {
+var draw = (e) => {
+
+}
+
+//var wipeCanvas = function() {
+var wipeCanvas = () => { //no need to have event for param
+
 }
 
 c.addEventListener("click", draw);
